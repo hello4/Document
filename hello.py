6feac3ebcdc3e5b0c3b0137b3515d3d2c6f6806f
hello4 <hello4@naver.com>
2022-09-14 12:50:16 +0900

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-// For command line git tutorial
-// first: Local repository modification
-// secodn : gitHub modification
-
-print("Hello World")
-print("Tell Your World")
-print("Tell his world")
-print("Tell her world")
-print("Tell my world")
